058e40464c7c91a112ac859bea5c637baf6fff55
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-04-19 00:47:19 +0100

    Add Move() method, Add readonly property Cost; Turned Unit class into abstract; Turned Propriety Health virtual in case we do not want to override it;

--- GameUnits/Unit.cs
+++ GameUnits/Unit.cs
@@ -1,14 +1,20 @@
 namespace GameUnits
 {
-    public class Unit
+    public abstract class Unit
     {
         private int movement;
-        public int Health { get; set; }
+        public virtual int Health { get; set; }
+        public abstract float Cost { get;}
 
         public Unit(int movement, int health)
         {
             this.movement = movement;
             Health = health;
         }
+        private void Move()
+        {
+            Console.WriteLine(movement);
+        }
+
     }
 }
