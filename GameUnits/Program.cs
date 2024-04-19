db9ee12e7ce3547245aef8e5bec5d1e9f54fd732
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-04-19 01:51:18 +0100

    Add test prints in program

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -6,7 +6,15 @@ namespace GameUnits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Unit MilitaryUnit mu = new MilitaryUnit(5,10,10);
+            Unit SettlerUnit su = new SettlerUnit(10,5);
+
+            mu.Move();
+            su.Move();
+
+            Console.WriteLine($"{mu.Health} {mu.Cost}");
+            Console.WriteLine($"{su.Health} {su.Cost}");
+
         }
     }
 }
