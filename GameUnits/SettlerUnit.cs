ca66142e446f306246637c4591b8080191f0df7d
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-04-19 01:46:22 +0100

    Add SettleUnit Class, Create constructor; Override Cost

---
+++ GameUnits/SettlerUnit.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameUnits
+{
+    public class SettlerUnit : Unit
+    {
+        public SettlerUnit():base(mov = 1,health = 3)
+
+        public override float Cost{get;} = 5
+    }
+}
