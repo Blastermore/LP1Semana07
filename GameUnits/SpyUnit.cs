a5a05388b59771bd0abeb38f2b3a2f366ff72772
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-04-19 13:30:50 +0100

    Test program

--- GameUnits/SpyUnit.cs
+++ GameUnits/SpyUnit.cs
@@ -5,8 +5,25 @@ using System.Threading.Tasks;
 
 namespace GameUnits
 {
-    public class SpyUnit
+    public class SpyUnit : XPUnit
     {
-        
+        public override float Cost{get;} = 12.5f;
+        public SpyUnit(int mov = 8, int health = 2) :base(mov,health){}
+
+        public void GetSecretInfo(Unit unit)
+        {
+            if(unit is SpyUnit)
+            {
+                XP += 3;
+            }
+            else if(unit is MilitaryUnit)
+            {
+                XP += 2;
+            }
+            else
+            {
+                XP += 1;
+            }
+        }
     }
 }
